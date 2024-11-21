03f9ca0c7a1b959790c583f9806d20bbad04f04c
hani <hani.lim@cheilpengtai.com>
2024-11-21 10:54:22 +0900

    [feat] webpush코드 이전

--- dist/webpush-react/Component.js
+++ dist/webpush-react/Component.js
@@ -1,4 +1,197 @@
-import { jsx as _jsx } from "react/jsx-runtime";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import { Button } from "antd";
+import axios from "axios";
+import { useEffect, useState } from "react";
+var LOCAL = "localhost";
+// const LOCAL = "182.198.46.77"; // machost
 export var Component = function () {
-    return _jsx("div", { children: "ujunglim test component" });
+    var _a = useState([]), messages = _a[0], setMessages = _a[1];
+    var _b = useState('{"title": "테스트2", "body": "메세지 전송 테스트2"}'), sendPushMessage = _b[0], setSendPushMessage = _b[1];
+    var _c = useState(), pushId = _c[0], setPushId = _c[1];
+    useEffect(function () {
+        var sessionPushId = sessionStorage.getItem("pushId");
+        console.log("sessionPushId: ", sessionPushId);
+        // pushId가 있으면 바로 sse연결
+        if (sessionPushId) {
+            setPushId(sessionPushId);
+            handleSSEConnect();
+        }
+    }, []);
+    var handleSSEConnect = function () { return __awaiter(void 0, void 0, void 0, function () {
+        var url, mocktoken, response, e_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    url = new URL("http://".concat(LOCAL, ":10041/noti/sse"));
+                    if (pushId) {
+                        url.searchParams.append("pushId", pushId);
+                    }
+                    mocktoken = "";
+                    return [4 /*yield*/, fetch(url.toString(), {
+                            method: "GET",
+                            credentials: "include",
+                            headers: {
+                                Accept: "text/event-stream",
+                                "Cache-Control": "no-cache",
+                                Connection: "keep-alive",
+                                Authorization: "Bearer ".concat(localStorage.getItem("authorization")),
+                                // Authorization: `Bearer ${mocktoken}`, // machost
+                            },
+                        })];
+                case 1:
+                    response = _a.sent();
+                    if (!response.ok) {
+                        throw new Error("HTTP error! status: ".concat(response.status));
+                    }
+                    handleStreamData(response);
+                    return [3 /*break*/, 3];
+                case 2:
+                    e_1 = _a.sent();
+                    console.error(e_1);
+                    return [3 /*break*/, 3];
+                case 3: return [2 /*return*/];
+            }
+        });
+    }); };
+    var handleStreamData = function (response) { return __awaiter(void 0, void 0, void 0, function () {
+        var reader, decoder, buffer, _a, value, done, lines, _i, lines_1, line, data;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    reader = response.body.getReader();
+                    decoder = new TextDecoder();
+                    buffer = "";
+                    _b.label = 1;
+                case 1:
+                    if (!true) return [3 /*break*/, 3];
+                    return [4 /*yield*/, reader.read()];
+                case 2:
+                    _a = _b.sent(), value = _a.value, done = _a.done;
+                    if (done) {
+                        console.log("Stream complete");
+                        return [3 /*break*/, 3];
+                    }
+                    // 스트림 데이터를 디코딩 후 버퍼에 저장
+                    buffer += decoder.decode(value, { stream: true });
+                    lines = buffer.split("\n");
+                    buffer = lines.pop() || "";
+                    for (_i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
+                        line = lines_1[_i];
+                        // 유효한 데이터 추출
+                        if (line.startsWith("data:")) {
+                            data = line.slice(5);
+                            handleRecievedData(data);
+                        }
+                    }
+                    return [3 /*break*/, 1];
+                case 3: return [2 /*return*/];
+            }
+        });
+    }); };
+    var handleRecievedData = function (data) {
+        console.log(data);
+        // 첫 연결
+        if (data.startsWith("Connected to")) {
+            var newPushId = data.split(":")[1];
+            setPushId(newPushId);
+            sessionStorage.setItem("pushId", newPushId);
+        }
+        else {
+            var pushId_1 = sessionStorage.getItem("pushId");
+            var parsedData_1 = JSON.parse(data);
+            if (parsedData_1.title && parsedData_1.body) {
+                setMessages(function (prevMessages) { return __spreadArray(__spreadArray([], prevMessages, true), [
+                    "".concat(pushId_1, ": ").concat(parsedData_1.body),
+                ], false); });
+                var notification = new Notification(parsedData_1.title, {
+                    body: "".concat(parsedData_1.body, " \npushID: ").concat(pushId_1),
+                    icon: "../favicon.ico",
+                });
+                // 알람클릭 시 이동
+                notification.addEventListener("click", function () {
+                    window.focus();
+                });
+            }
+        }
+    };
+    var handleSendMessage = function () { return __awaiter(void 0, void 0, void 0, function () {
+        var res, e_2;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    return [4 /*yield*/, axios.post("http://".concat(LOCAL, ":10041/noti/send"), {
+                            notificationType: "WEBPUSH",
+                            pushId: pushId,
+                            content: sendPushMessage,
+                        })];
+                case 1:
+                    res = _a.sent();
+                    return [3 /*break*/, 3];
+                case 2:
+                    e_2 = _a.sent();
+                    console.error(e_2);
+                    return [3 /*break*/, 3];
+                case 3: return [2 /*return*/];
+            }
+        });
+    }); };
+    return (_jsxs("div", { children: [_jsx("h1", { children: "Fetch SSE test " }), _jsx("input", { type: "text", value: sendPushMessage, style: { width: "500px" }, onChange: function (event) { return setSendPushMessage(event.target.value); } }), _jsx("br", {}), _jsx(Button, __assign({ onClick: handleSSEConnect }, { children: "SSE connect" })), _jsx(Button, __assign({ onClick: handleSendMessage }, { children: "Send Message" })), _jsx("p", { children: "SSE \uC5F0\uACB0\uB428: ".concat(pushId ? "O" : "X") }), _jsxs("p", { children: ["push Id: ", pushId] }), messages.map(function (msg, i) { return (_jsx("li", { children: msg }, i)); })] }));
 };
--- src/lib/webpush-react/Component.tsx
+++ src/lib/webpush-react/Component.tsx
@@ -1,3 +1,141 @@
+import { Button } from "antd";
+import axios from "axios";
+import { useEffect, useState } from "react";
+const LOCAL = "localhost";
+// const LOCAL = "182.198.46.77"; // machost
+
 export const Component = () => {
-  return <div>ujunglim test component</div>;
+  const [messages, setMessages] = useState<string[]>([]);
+  const [sendPushMessage, setSendPushMessage] = useState(
+    '{"title": "테스트2", "body": "메세지 전송 테스트2"}'
+  );
+  const [pushId, setPushId] = useState<string>();
+
+  useEffect(() => {
+    const sessionPushId = sessionStorage.getItem("pushId");
+    console.log("sessionPushId: ", sessionPushId);
+    // pushId가 있으면 바로 sse연결
+    if (sessionPushId) {
+      setPushId(sessionPushId);
+      handleSSEConnect();
+    }
+  }, []);
+
+  const handleSSEConnect = async () => {
+    try {
+      const url = new URL(`http://${LOCAL}:10041/noti/sse`);
+      if (pushId) {
+        url.searchParams.append("pushId", pushId);
+      }
+      const mocktoken = "";
+      const response = await fetch(url.toString(), {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Accept: "text/event-stream",
+          "Cache-Control": "no-cache",
+          Connection: "keep-alive",
+          Authorization: `Bearer ${localStorage.getItem("authorization")}`,
+          // Authorization: `Bearer ${mocktoken}`, // machost
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      handleStreamData(response);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const handleStreamData = async (response: any) => {
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder();
+    let buffer = "";
+
+    // 스트림 데이터를 비동기적으로 반복 읽기
+    while (true) {
+      const { value, done } = await reader.read();
+
+      if (done) {
+        console.log("Stream complete");
+        break;
+      }
+
+      // 스트림 데이터를 디코딩 후 버퍼에 저장
+      buffer += decoder.decode(value, { stream: true });
+      // 데이터를 줄로 나누고, 완전하지 않은 마지막 줄은 다시 버퍼에 저장
+      const lines = buffer.split("\n");
+      buffer = lines.pop() || "";
+
+      for (const line of lines) {
+        // 유효한 데이터 추출
+        if (line.startsWith("data:")) {
+          const data = line.slice(5);
+          handleRecievedData(data);
+        }
+      }
+    }
+  };
+
+  const handleRecievedData = (data: string) => {
+    console.log(data);
+    // 첫 연결
+    if (data.startsWith("Connected to")) {
+      const newPushId: string = data.split(":")[1];
+      setPushId(newPushId);
+      sessionStorage.setItem("pushId", newPushId);
+    } else {
+      const pushId = sessionStorage.getItem("pushId");
+      const parsedData = JSON.parse(data);
+      if (parsedData.title && parsedData.body) {
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          `${pushId}: ${parsedData.body}`,
+        ]);
+
+        const notification = new Notification(parsedData.title, {
+          body: `${parsedData.body} \npushID: ${pushId}`,
+          icon: "../favicon.ico",
+        });
+        // 알람클릭 시 이동
+        notification.addEventListener("click", () => {
+          window.focus();
+        });
+      }
+    }
+  };
+
+  const handleSendMessage = async () => {
+    try {
+      const res = await axios.post(`http://${LOCAL}:10041/noti/send`, {
+        notificationType: "WEBPUSH",
+        pushId,
+        content: sendPushMessage,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Fetch SSE test </h1>
+      <input
+        type="text"
+        value={sendPushMessage}
+        style={{ width: "500px" }}
+        onChange={(event) => setSendPushMessage(event.target.value)}
+      />
+      <br />
+      <Button onClick={handleSSEConnect}>SSE connect</Button>
+      <Button onClick={handleSendMessage}>Send Message</Button>
+      <p>{`SSE 연결됨: ${pushId ? "O" : "X"}`}</p>
+      <p>push Id: {pushId}</p>
+      {messages.map((msg, i) => (
+        <li key={i}>{msg}</li>
+      ))}
+    </div>
+  );
 };
